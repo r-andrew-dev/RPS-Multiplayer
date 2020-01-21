88eb46cd9e0db9a046459beeb65e8ac3e8d7ba2c
Rachael Andrew <r.andrew.dev@gmail.com>
2020-01-20 21:21:57 -0600

    added and styled score div, changed all variable declarations to let and const

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,19 +2,19 @@
 // When button is clicked, player added will need to be established in the database 
 // (could this be as simple as a click count?  -- No, refreshing would not change the clickcount, could create discrepencies)
 // AND will need to check if there are any players present, and if so how many. 
-// If there is one player present, user would be added to current session. +++ AND scren displaying "waiting for another player to join... " would display. A function would run 
+// If there is one player present, user would be added to current session. +++ AND scren displaying "waiting for another player to join... " would display. A function would run [USE FIREBASE BUILT IN ON VALUE CHANGE RULES]
 // (There also needs to be some piece of code checking if one of the players tries to close out of the session, maybe ask if they really want to leave, and then update the number of players, accordingly)
 // If there are two players present, a new session would need to be created. 
 // One the next screen, user will see choices. "bird", "apple", "worm".
 // User makes a choice. On button click, screen will show user their choice with a picture and also push that choice to the database.
-// Then every second a function will run to check wether other choice has been made. 
+// Then every second a function [USE DATABASE .ref INSTEAD OF SET INVTERVAL WITH FUNCTION] will run to check wether other choice has been made. 
 // Once other choice is made, the computer will need to pull the two choices and run the comparison. 
 //  Bird eats worm, worm eats apple, apple crushes bird. 
 // Game should display the results to the user, tally them a win or loss appropriately, show them a play again? or Done, button. Selecting Play again should take them back
 // to CHOICE screen with wins/losses still displaying whereas selecting Done. should take them back to welcome screen and score is reset and they are removed from the database. (player does not need to see this).
 
 
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyDsuTaxT1Wc8hrJB69jUMwVCJ_M1f8dQow",
     authDomain: "rps-multiplayer-2ee79.firebaseapp.com",
     databaseURL: "https://rps-multiplayer-2ee79.firebaseio.com",
@@ -26,7 +26,17 @@ var firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-  var database = firebase.database();
+  let score = {
+    wins: 0,
+    losses: 0,
+  }
+
+  let playerCount = 0;
+  let choice1;
+  let choice2;
+
+
+  const dB = firebase.database();
 
   $("#start-button").on("click", function() {
 
@@ -34,24 +44,25 @@ var firebaseConfig = {
 
     $("#start-div").hide();
 
-    chooseDiv = $("<div id='choose-div'>");
-
-    chooseHead = $("<h2 class='font-effect-3d-float' id='choice-text'>");
-
+    const chooseDiv = $("<div id='choose-div'>");
+    const chooseHead = $("<h2 class='font-effect-3d-float' id='choice-text'>");
     chooseHead.text("CHOOSE ... ");
-
     chooseDiv.append(chooseHead);
-
-    var choices = ["Bird", "Apple", "Worm"];
-
+    const choices = ["Bird", "Apple", "Worm"];
     for (i = 0; i < choices.length; i++) {
       button = $("<button class='button choice' id='Choice-" + i + "'><br>");
       button.text(choices[i])
+      button.attr("choice", choices[i]);
       chooseDiv.append(button);
       chooseDiv.append("<br>")
 
     }
 
+    const scoreDiv = $("<div id='score-div'>");
+    scoreDiv.html("<p id='score'>Score</p><p>Wins: " + score.wins + "</p><p>Losses: " + score.losses + "</p>");
+
     $("#container").append(chooseDiv);
+    $("#container").append(scoreDiv);
     
     });
+
